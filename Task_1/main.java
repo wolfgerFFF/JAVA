9103e4f492552bd24f05922805ee08a2e70d75c9
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-02 19:39:01 +0300

    Update main.java

--- Task_1/main.java
+++ Task_1/main.java
@@ -1,1 +1,28 @@
+public static void main(String[] args) {
+
+   Market market = new Market();
+
+    market.EnteredTheQueue("Олег");
+    market.it_ordered("Заказ №1");
+    market.update();
+    market.ExitTheQueue();
+    market.in_order();
+
+    market.update();
+    market.EnteredTheQueue("Иван");
+    market.it_ordered("Заказ №2");
+    market.update();
+    market.ExitTheQueue();
+    market.in_order();
+
+
+    market.update();
+    market.EnteredTheQueue("Катя");
+    market.it_ordered("Заказ №3");
+    market.update();
+    market.ExitTheQueue();
+    market.in_order();
+    
+
+}
 
