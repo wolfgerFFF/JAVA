861a23ffea88ca17b0b6d69eab3bf7a0ac2c1e9c
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-19 00:57:52 +0300

    Create UserService.java

---
+++ src/java/UserService.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface UserService<T extends User> {
+    public List<T> getAll();
+    public void initData(List<T> list);
+    public void create(String surname, String firstname, String patronymic);
+}
