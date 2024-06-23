8ffdd2c59db8d77214d88d2df7651d1e0596fc00
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-23 20:46:46 +0300

    Create StudentView.java

---
+++ Task_5/View/StudentView.java
@@ -0,0 +1,12 @@
+import Task_5.model.Student;
+
+import java.util.List;
+
+public class StudentView implements UserView<Student> {
+    @Override
+    public void sendOnConsole(List<Student> list) {
+        for (Student student : list) {
+            System.out.println(student);
+        }
+    }
+}
