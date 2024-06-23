4df030d8a9ce6fd22b943c11f1682dbf33ff9305
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-23 20:25:48 +0300

    Create Student.java

---
+++ Task_5/Model/Student.java
@@ -0,0 +1,21 @@
+public class Student extends User {
+    private Integer studentId;
+
+    public Student(Integer studentId, String surname, String firstname, String patronymic) {
+        super(surname, firstname, patronymic);
+        this.studentId=studentId;
+    }
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "teacherId=" + studentId +
+                "surname='" + super.getSurname() + '\'' +
+                ", firstname='" + super.getFirstname() + '\'' +
+                ", patronymic='" + super.getPatronymic() + '\'' +
+                '}';
+    }
+}
