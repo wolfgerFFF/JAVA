51f57a4c9ccdf113ebc98e521cc36666bf2ffa97
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-23 20:34:26 +0300

    Create StudentGrupService.java

---
+++ Task_5/Service/StudentGrupService.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class StudentGroupService {
+    private final StudentGroup studentGroup = new StudentGroup();
+    private Integer maxGroupId = 0;
+    public void createStudentGroup(Teacher teacher, List<Student> studentList){
+        studentGroup.setGroupId(++maxGroupId);
+        studentGroup.setTeacher(teacher);
+        studentGroup.setStudentList(studentList);
+    }
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+}
