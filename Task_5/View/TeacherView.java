29cbf1093406710d4dca518da76a2b841429af5a
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-23 21:16:57 +0300

    Create TeacherView.java

---
+++ Task_5/View/TeacherView.java
@@ -0,0 +1,14 @@
+package Task_5.View;
+
+import Task_5.Model.Teacher;
+
+import java.util.List;
+
+public class TeacherView implements UserView<Teacher> {
+    @Override
+    public void sendOnConsole(List<Teacher> list) {
+        for (Teacher teacher : list) {
+            System.out.println(teacher);
+        }
+    }
+}
