a4bd74f04c27426cef4e81bdd54c81d2abafe7e5
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-30 22:57:54 +0300

    Create FileLogger.java

---
+++ Task_07/Ex_07/Calculator/FileLogger.java
@@ -0,0 +1,30 @@
+package Task_07.Ex_07.calculator;
+
+import java.util.logging.*;
+
+public class FileLogger implements Loggable{
+    private final Logger logger = Logger.getLogger("MyLog");
+
+    public FileLogger() {
+        setFileHandler("log.txt");
+    }
+
+    public FileLogger(String fileNameLog) {
+        setFileHandler(fileNameLog);
+    }
+    public void setFileHandler(String fileNameLog){
+        this.logger.setUseParentHandlers(false);
+        try{
+            FileHandler fh = new FileHandler(fileNameLog, true);
+            this.logger.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+        } catch(Exception e) {
+            System.out.println("Ошибка работы с файлом.");
+            System.out.println(e.getMessage());
+        }
+    }
+    @Override
+    public void log(String msg) {
+        logger.info(msg);
+    }
+}
