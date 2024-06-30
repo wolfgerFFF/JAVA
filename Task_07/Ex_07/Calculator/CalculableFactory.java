a75f7562a748a3c15e67ef69e17e67b62904fdac
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-06-30 22:53:34 +0300

    Create CalculableFactory.java

---
+++ Task_07/Ex_07/Calculator/CalculableFactory.java
@@ -0,0 +1,10 @@
+package Task_07.Ex_07.calculator;
+
+import Task_07.Ex_07.complex.Complex;
+
+public class CalculableFactory implements ICalculableFactory{
+    @Override
+    public Calculable create(Complex firstArg) {
+        return new Calculator(firstArg);
+    }
+}
